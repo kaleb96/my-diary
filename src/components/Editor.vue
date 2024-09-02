ce14e3378e2fd23e21fe17c1466195fae8ed98b9
박기윤 <yes505304@ipactory.com>
2024-09-02 17:55:16 +0900

    feat: 워크스페이스 에디터 등록

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,6 +1,34 @@
 <template>
-
+    <div>
+      <div ref="editorRef" style="height: 400px"></div>
+    </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+
+import { ref, onMounted, watch} from 'vue'
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+
+// Refs
+const editor = ref('');
+const editorRef = ref(null);
+
+const toolbarOptions = [
+  [{'header': [1, 2, 3, 4, 5, 6]}],
+  [{'font': []}],
+  [{'color': []}, {'background': []}],
+  [{'align': []}],
+  [{'indent': '-1'}, {'indent': '+1'}],
+  ['bold', 'italic', 'underline', 'strike']
+]
+
+onMounted(() => {
+  editor.value = new Quill(editorRef.value, {
+    theme: 'snow',
+    placeholder: '이야기를 입력하세요.',
+    modules: {
+      toolbar: toolbarOptions
+    }
+  })
+})
 </script>
